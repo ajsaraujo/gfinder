1bf771084204e6b8e736710b40ec6f3eba6ee8d4
Allan <allanjuansil@gmail.com>
2020-05-17 14:14:07 -0300

    Change actions ui

--- lib/views/single_gif_page.dart
+++ lib/views/single_gif_page.dart
@@ -4,39 +4,48 @@ import 'package:share/share.dart';
 
 class SingleGifPage extends StatelessWidget {
   final Map gifData;
-  
+
   SingleGifPage({this.gifData});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Gfinder', style: GoogleFonts.roboto()),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Gfinder', style: GoogleFonts.roboto()),
+          backgroundColor: Colors.black,
+        ),
         backgroundColor: Colors.black,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: () {
-              Share.share(gifData['images']['fixed_height']['url']);
-            }
-          )
-        ],
-      ),
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(gifData['title'], textAlign: TextAlign.center, style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
-          )),
-          SizedBox(height: 10.0),
-          Center(
-            child: Image.network(gifData['images']['fixed_height']['url'])
-          )
-        ],
-      )
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(gifData['title'],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                )),
+            SizedBox(height: 20.0),
+            Image.network(gifData['images']['fixed_height']['url']),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.favorite_border,
+                      color: Colors.white, size: 50.0),
+                  onPressed: () {
+                    print('${gifData['title']} was marked as a favorite!');
+                  },
+                ),
+                IconButton(
+                    icon: Icon(Icons.share, color: Colors.white, size: 50.0),
+                    onPressed: () {
+                      Share.share(gifData['images']['fixed_height']['url']);
+                    })
+              ],
+            )
+          ],
+        ));
   }
 }

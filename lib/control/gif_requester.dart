a976ccbf4cc7f83081fa737a8529b42f77c4ce9d
Allan <allanjuansil@gmail.com>
2020-05-17 13:19:28 -0300

    Make api key an environment variable

--- lib/control/gif_requester.dart
+++ lib/control/gif_requester.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class GifRequester {
   String _lastSearch;
   int _offset = 0;
-  final int _numOfGifs = 19;
 
+  final int _numOfGifs = 19;
+  final _apiKey = DotEnv().env['API_KEY'];
+  
   static final GifRequester _singleton = GifRequester._internal();
 
   factory GifRequester() {
@@ -16,11 +19,11 @@ class GifRequester {
   GifRequester._internal();
 
   String _trendingGifsEndpoint() {
-    return 'https://api.giphy.com/v1/gifs/trending?api_key=01BdxRWujnufHK49HzxbDee0BSilRkwI&limit=${_numOfGifs}&rating=G';
+    return 'https://api.giphy.com/v1/gifs/trending?api_key=$_apiKey&limit=${_numOfGifs}&rating=G';
   }
 
   String _searchGifsEndpoint() {
-    return 'https://api.giphy.com/v1/gifs/search?api_key=01BdxRWujnufHK49HzxbDee0BSilRkwI&q=${_lastSearch}&limit=${_numOfGifs}&offset=${_offset}&rating=G&lang=en';
+    return 'https://api.giphy.com/v1/gifs/search?api_key=$_apiKey&q=${_lastSearch}&limit=${_numOfGifs}&offset=${_offset}&rating=G&lang=en';
   }
 
   void _updateOffsetAndSearch(String search) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'views/home_page.dart';
 
-void main() {
+Future main() async {
+  await DotEnv().load('.env');
+  
   runApp(MaterialApp(
     home: HomePage(),
     theme: ThemeData(hintColor: Colors.white)

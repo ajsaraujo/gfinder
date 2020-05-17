72fab0867f2e01957fa310b160c36802ec1b22bb
Allan <allanjuansil@gmail.com>
2020-05-17 12:49:18 -0300

    Refactor HomePage's Scaffold

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gfinder/control/navigation_mode.dart';
-import 'package:gfinder/views/single_gif_page.dart';
-import 'package:gfinder/widgets/gif_grid_item.dart';
 import 'package:gfinder/widgets/gif_grid_view.dart';
-import 'package:gfinder/widgets/load_more_gifs_button.dart';
-import 'package:share/share.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 import 'package:gfinder/control/gif_requester.dart';
 
@@ -20,33 +15,47 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          title: Text('GFINDER',
-              style: TextStyle(
-                color: Colors.white,
-              )),
+    final _myAppBar = AppBar(
+      backgroundColor: Colors.black,
+      centerTitle: true,
+      title: Text('GFINDER',
+          style: TextStyle(
+            color: Colors.white,
+          )),
+    );
+
+    final _searchField = Padding(
+      padding: EdgeInsets.all(10.0),
+      child: TextField(
+        style: TextStyle(color: Colors.white, fontSize: 18.0),
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          labelText: 'Search GIFs',
+          labelStyle: TextStyle(color: Colors.white),
+          border: OutlineInputBorder(),
         ),
+        onSubmitted: (String value) {
+          setState(() {
+            _search = value;
+          });
+        }));
+
+    final _progressIndicatorContainer = Container(
+      width: 200.0,
+      height: 200.0,
+      alignment: Alignment.center,
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 5.0,
+      ),
+    );
+
+    return Scaffold(
+        appBar: _myAppBar,
         backgroundColor: Colors.black,
         body: Column(
           children: <Widget>[
-            Padding(
-                padding: EdgeInsets.all(10.0),
-                child: TextField(
-                    onSubmitted: (String value) {
-                      setState(() {
-                        _search = value;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Search GIFs',
-                      labelStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(),
-                    ),
-                    style: TextStyle(color: Colors.white, fontSize: 18.0),
-                    textAlign: TextAlign.center)),
+            _searchField,
             Expanded(
                 child: FutureBuilder(
                     future: _gifRequester.fetchGifs(_search),
@@ -62,43 +71,10 @@ class _HomePageState extends State<HomePage> {
                                 setState(() {});
                               }));
                         default:
-                          return Container(
-                            width: 200.0,
-                            height: 200.0,
-                            alignment: Alignment.center,
-                            child: CircularProgressIndicator(
-                              valueColor:
-                                  AlwaysStoppedAnimation<Color>(Colors.white),
-                              strokeWidth: 5.0,
-                            ),
-                          );
+                          return _progressIndicatorContainer;
                       }
                     }))
           ],
         ));
-  }
-
-  _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
-    int _itemCount = snapshot.data['data'].length;
-    if (_search != null) _itemCount++;
-
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10.0,
-        mainAxisSpacing: 10.0,
-      ),
-      itemCount: _itemCount,
-      itemBuilder: (context, index) {
-        if (_search == null || index < snapshot.data['data'].length) {
-          return GifGridItem(gifData: snapshot.data['data'][index]);
-        } else {
-          return LoadMoreGifsButton(redrawHomePage: () {
-            setState(() {});
-          });
-        }
-      },
-      padding: EdgeInsets.all(10.0),
-    );
   }
 }
